cea15beab3855a1a1af6c6b67fdfd82a2a60d9a3
MatteoAba <matteoabaterusso@gmail.com>
2022-05-11 13:29:19 +0200

    Added basic Review's CRUD and Tests

--- src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/persistence/mongo/ReviewMongo.java
+++ src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/persistence/mongo/ReviewMongo.java
@@ -1,19 +1,20 @@
 package it.unipi.dii.lsmsdb.myPodcastDB.persistence.mongo;
 
-import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
+import static com.mongodb.client.model.Updates.set;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Filters.eq;
 import it.unipi.dii.lsmsdb.myPodcastDB.model.Review;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
-
+import com.mongodb.client.MongoCursor;
+import org.bson.Document;
 
 import java.util.Date;
 import java.util.List;
-
-import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Updates.set;
-import static com.mongodb.client.model.Updates.combine;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class ReviewMongo {
 
@@ -22,21 +23,138 @@ public class ReviewMongo {
     // --------- CREATE --------- //
 
     public boolean addReview(Review review) {
-        return false;
+        MongoManager manager = MongoManager.getInstance();
+
+        Document newReview = new Document()
+                .append("podcastId", new ObjectId(review.getPodcastId()))
+                .append("title", review.getTitle())
+                .append("content", review.getContent())
+                .append("rating", review.getRating())
+                .append("createdAt", review.getCreatedAtAsString())
+                .append("authorUsername", review.getAuthorUsername());
+
+        try {
+            manager.getCollection("review").insertOne(newReview);
+            review.setId(newReview.getObjectId("_id").toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     // ---------- READ ---------- //
 
     public Review findReviewById(String id) {
+        MongoManager manager = MongoManager.getInstance();
+
+        try (MongoCursor<Document> cursor = manager.getCollection("review").find(eq("_id", new ObjectId(id))).iterator()) {
+            if (cursor.hasNext()) {
+                Document review = cursor.next();
+
+                // review attributes
+                String podcastId = review.getObjectId("podcastId").toString();
+                String title = review.getString("title");
+                String content = review.getString("content");
+                int rating = review.getInteger("rating");
+                String strCreatedAt = review.getString("createdAt").replace("T", " "). replace("Z", "");
+                Date createdAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(strCreatedAt);
+                String authorName = review.getString("authorName");
+
+                Review newReview = new Review(id, podcastId, authorName, title, content, rating, createdAt);
+                return newReview;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
         return null;
     }
 
     public List<Review> findReviewsByPodcastId(String podcastId, int limit, String attributeToOrder, boolean ascending) {
-        return null;
+        MongoManager manager = MongoManager.getInstance();
+
+        try (MongoCursor<Document> cursor = manager.getCollection("review").find(eq("podcastId", new ObjectId(podcastId))).limit(limit).iterator()) {
+            List<Review> reviews = new ArrayList<>();
+
+            while (cursor.hasNext()) {
+                Document review = cursor.next();
+
+                // review attributes
+                String id = review.getObjectId("_id").toString();
+                String title = review.getString("title");
+                String content = review.getString("content");
+                int rating = review.getInteger("rating");
+                String strCreatedAt = review.getString("createdAt").replace("T", " "). replace("Z", "");
+                Date createdAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(strCreatedAt);
+                String authorUsername = review.getString("authorUsername");
+
+                Review newReview = new Review(id, podcastId, authorUsername, title, content, rating, createdAt);
+                reviews.add(newReview);
+            }
+            
+            return reviews;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public List<Review> findReviewsByAuthorUsername(String authorUsername, int limit, String attributeToOrder, boolean ascending) {
-        return null;
+        MongoManager manager = MongoManager.getInstance();
+
+        try (MongoCursor<Document> cursor = manager.getCollection("review").find(eq("authorUsername", authorUsername)).limit(limit).iterator()) {
+            List<Review> reviews = new ArrayList<>();
+
+            while (cursor.hasNext()) {
+                Document review = cursor.next();
+
+                // review attributes
+                String id = review.getObjectId("_id").toString();
+                String podcastId = review.getObjectId("podcastId").toString();
+                String title = review.getString("title");
+                String content = review.getString("content");
+                int rating = review.getInteger("rating");
+                String strCreatedAt = review.getString("createdAt").replace("T", " "). replace("Z", "");
+                Date createdAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(strCreatedAt);
+
+                Review newReview = new Review(id, podcastId, authorUsername, title, content, rating, createdAt);
+                reviews.add(newReview);
+            }
+
+            /* / order by rating
+            if (attributeToOrder.equals("rating")) {
+                if (ascending) {
+                    reviews.sort((Review r1, Review r2) -> {
+                        if (r1.getRating() > r2.getRating())
+                            return -1;
+                        else if (r1.getRating() == r2.getRating())
+                            return 0;
+                        return 1;
+                    });
+                } else {
+                    reviews.sort((Review r1, Review r2) -> {
+                        if (r1.getRating() < r2.getRating())
+                            return -1;
+                        else if (r1.getRating() == r2.getRating())
+                            return 0;
+                        return 1;
+                    });
+                }
+            } else if (attributeToOrder.equals("createdAt")) {
+                if (ascending)
+                    reviews.sort((Review r1, Review r2) -> r1.getCreatedAt().compareTo(r2.getCreatedAt()));
+                else
+                    reviews.sort((Review r1, Review r2) -> r2.getCreatedAt().compareTo(r1.getCreatedAt()));
+            }*/
+
+            return reviews;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // --------- UPDATE --------- //
--- src/test/java/it/unipi/dii/lsmsdb/myPodcastDB/persistence/mongo/ReviewMongoTest.java
+++ src/test/java/it/unipi/dii/lsmsdb/myPodcastDB/persistence/mongo/ReviewMongoTest.java
@@ -3,19 +3,57 @@ package it.unipi.dii.lsmsdb.myPodcastDB.persistence.mongo;
 import it.unipi.dii.lsmsdb.myPodcastDB.model.Review;
 
 import java.util.Date;
+import java.util.List;
 
 public class ReviewMongoTest {
 
-    ReviewMongo rm;
+    // TODO: aggiungere test add e rendere i test consistenti (add - test - delete)
+    ReviewMongo reviewMongo;
 
     public ReviewMongoTest() {
-        this.rm = new ReviewMongo();
+        this.reviewMongo = new ReviewMongo();
+    }
+
+    public void findByIdTest() {
+        Review review = this.reviewMongo.findReviewById("000000000000000000000000");
+        if (review.getPodcastId().equals("7d7281ba3edb77da6a9fe95c") && review.getTitle().equals("Best podcast like, Ever."))
+            System.out.println("[+] findReviewById");
+        else
+            System.err.println("[-] findReviewById");
+    }
+
+    public void findTest() {
+        List<Review> reviews = this.reviewMongo.findReviewsByAuthorUsername("angrybear104838", 2, "", true);
+        if (reviews.size() != 2)
+            System.err.println("[-] findReviewsByAuthorUsername: limit doesn't work");
+        boolean test = true;
+        for (Review review : reviews) {
+            if (!review.getAuthorUsername().equals("angrybear104838")) {
+                System.err.println("[-] findReviewsByAuthorUsername");
+                test = false;
+            }
+        }
+        if (test)
+            System.out.println("[+] findReviewsByAuthorUsername");
+
+        reviews = this.reviewMongo.findReviewsByPodcastId("0005852e3f81889c1ff0f26c", 4, "", true);
+        if (reviews.size() != 4)
+            System.err.println("[-] findReviewsByPodcastId: limit doesn't work");
+        test = true;
+        for (Review review : reviews) {
+            if (!review.getPodcastId().equals("0005852e3f81889c1ff0f26c")) {
+                System.err.println("[-] findReviewsByPodcastId");
+                test = false;
+            }
+        }
+        if (test)
+            System.out.println("[+] findReviewsByPodcastId");
     }
 
     public void deleteReviewByIdTest() {
         // Review newReview = addReviewForTest();
 
-        if(rm.deleteReviewById("000000000000000000000002"))
+        if(reviewMongo.deleteReviewById("000000000000000000000002"))
             System.out.println("[+] deleteReviewById");
         else
             System.err.println("[-] deleteReviewById");
@@ -23,7 +61,7 @@ public class ReviewMongoTest {
 
     public void deleteReviewsByPodcastIdTest() {
         // Review newReview = addReviewForTest();
-        int deletedCount = rm.deleteReviewsByPodcastId("7d7281ba3edb77da6a9fe95c");
+        int deletedCount = reviewMongo.deleteReviewsByPodcastId("7d7281ba3edb77da6a9fe95c");
 
         if (deletedCount > 0)
             System.out.println("[+] deleteReviewsByPodcastId - Deleted " + deletedCount);
@@ -33,7 +71,7 @@ public class ReviewMongoTest {
 
     public void deleteReviewsByAuthorUsernameTest() {
         // Review newReview = addReviewForTest();
-        int deletedCount = rm.deleteReviewsByAuthorUsername("smallmeercat795128");
+        int deletedCount = reviewMongo.deleteReviewsByAuthorUsername("smallmeercat795128");
 
         if (deletedCount > 0)
             System.out.println("[+] deleteReviewsByAuthorUsername - Deleted " + deletedCount);
@@ -46,24 +84,24 @@ public class ReviewMongoTest {
         Date dateReview = new Date();
         Review newReview = new Review("000000000000000000000003", "b09ab608dd57d787d28980e9", "organicmouse599943", "titleReview", "We buonasera", 5, dateReview);
 
-        if (rm.updateReview(newReview))
+        if (reviewMongo.updateReview(newReview))
             System.out.println("[+] updateReview");
         else
             System.err.println("[-] updateReview");
     }
-
+    
     public static void main(String[] args) {
-        System.out.println("Test Review");
-
-        MongoManager mongoManager = MongoManager.getInstance();
-        mongoManager.openConnection();
+        MongoManager manager = MongoManager.getInstance();
+        manager.openConnection();
         ReviewMongoTest test = new ReviewMongoTest();
 
+        test.findByIdTest();
+        test.findTest();
         test.deleteReviewByIdTest();
         test.deleteReviewsByPodcastIdTest();
         test.deleteReviewsByAuthorUsernameTest();
         test.updateReviewTest();
 
-        mongoManager.closeConnection();
+        manager.closeConnection();
     }
 }
