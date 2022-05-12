30af1ab0666428f387145f4daf8f893982acdadb
MatteoAba <matteoabaterusso@gmail.com>
2022-05-12 12:23:34 +0200

    Modified Podcast's Tests

--- src/test/java/it/unipi/dii/lsmsdb/myPodcastDB/persistence/mongo/PodcastMongoTest.java
+++ src/test/java/it/unipi/dii/lsmsdb/myPodcastDB/persistence/mongo/PodcastMongoTest.java
@@ -9,15 +9,27 @@ public class PodcastMongoTest {
     }
 
     void deleteAllEpisodesTest() {
-        this.podcastMongo.deleteAllEpisodesOfPodcast("9aaae9ac725c3a586701abf4");
+        boolean test = this.podcastMongo.deleteAllEpisodesOfPodcast("9aaae9ac725c3a586701abf4");
+        if (test)
+            System.out.println("[+] deleteAllEpisodesOfPodcast");
+        else
+            System.err.println("[-] deleteAllEpisodesOfPodcast");
     }
 
     void deleteReviewTest() {
-        this.podcastMongo.deleteReviewOfPodcast("34e734b09246d17dc5d56f63", "000000000000000000080116");
+        boolean test = this.podcastMongo.deleteReviewOfPodcast("34e734b09246d17dc5d56f63", "000000000000000000080116");
+        if (test)
+            System.out.println("[+] deleteReviewOfPodcast");
+        else
+            System.err.println("[-] deleteReviewOfPodcast");
     }
 
     void deleteAllReviewsTest() {
-        this.podcastMongo.deleteAllReviewsOfPodcast("9aaae9ac725c3a586701abf4");
+        boolean test = this.podcastMongo.deleteAllReviewsOfPodcast("9aaae9ac725c3a586701abf4");
+        if (test)
+            System.out.println("[+] deleteAllReviewsOfPodcast");
+        else
+            System.err.println("[-] deleteAllReviewsOfPodcast");
     }
 
     public static void main(String[] args) {
