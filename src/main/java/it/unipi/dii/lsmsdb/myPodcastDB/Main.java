d8671fea103e62ed1fdd7a7794570898c6b503f7
Matteo Abaterusso <matteoabaterusso@gmail.com>
2022-05-17 16:06:21 +0200

    Created Test Interface

--- src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/Main.java
+++ src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/Main.java
@@ -1,8 +1,24 @@
 package it.unipi.dii.lsmsdb.myPodcastDB;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("src/java/it/unipi/dii/lsmsdb/myPodcastDB/view/Test.fxml"));
+        primaryStage.setTitle("myPodcastDB");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         MyPodcastDB myPodcastDB = new MyPodcastDB();
         myPodcastDB.run();
+        Application.launch(args);
     }
 }
