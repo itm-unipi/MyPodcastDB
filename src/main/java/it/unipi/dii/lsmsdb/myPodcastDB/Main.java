64bb2727407b4c7f2bea585756a5f6f67bfb404d
MatteoAba <matteoabaterusso@gmail.com>
2022-05-18 19:52:36 +0200

    JavaFX works

--- src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/Main.java
+++ src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/Main.java
@@ -9,16 +9,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("src/java/it/unipi/dii/lsmsdb/myPodcastDB/view/Test.fxml"));
-        primaryStage.setTitle("myPodcastDB");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Test.fxml"));
+        stage.setTitle("myPodcastDB");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     public static void main(String[] args) {
         MyPodcastDB myPodcastDB = new MyPodcastDB();
         myPodcastDB.run();
-        Application.launch(args);
+        launch();
     }
 }
