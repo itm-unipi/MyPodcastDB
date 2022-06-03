84416de35480357b3e08a8154e2a2c56c1d92f16
Matteo Abaterusso <matteoabaterusso@gmail.com>
2022-06-03 16:35:31 +0200

    Created Podcast Page's services

--- src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/service/PodcastService.java
+++ src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/service/PodcastService.java
@@ -1,6 +1,7 @@
 package it.unipi.dii.lsmsdb.myPodcastDB.service;
 
 import it.unipi.dii.lsmsdb.myPodcastDB.MyPodcastDB;
+import it.unipi.dii.lsmsdb.myPodcastDB.model.Episode;
 import it.unipi.dii.lsmsdb.myPodcastDB.model.Podcast;
 import it.unipi.dii.lsmsdb.myPodcastDB.model.User;
 import it.unipi.dii.lsmsdb.myPodcastDB.persistence.mongo.MongoManager;
@@ -47,6 +48,10 @@ public class PodcastService {
         MongoManager.getInstance().openConnection();
 
         Podcast foundPodcast = this.podcastMongo.findPodcastById(podcast.getId());
+        if (foundPodcast == null) {
+            Logger.error("Podcast to update not found");
+            return;
+        }
         podcast.copy(foundPodcast);
 
         MongoManager.getInstance().closeConnection();
@@ -93,11 +98,103 @@ public class PodcastService {
     }
 
     public void updatePodcast(Podcast podcast) {
+        MongoManager.getInstance().openConnection();
 
+        // check if podcast exists
+        Podcast foundPodcast = this.podcastMongo.findPodcastById(podcast.getId());
+        if (foundPodcast == null) {
+            Logger.error("Podcast to update not found");
+            return;
+        }
+
+        // update podcast
+        Boolean result = this.podcastMongo.updatePodcast(podcast);
+        if (result) {
+            Logger.success("Podcast updated");
+        } else {
+            // reset the podcast object to the old status (for interface update)
+            Logger.error("Podcast not updated");
+            podcast.copy(foundPodcast);
+        }
+        MongoManager.getInstance().closeConnection();
     }
 
     public void deletePodcast(String podcastId) {
+        MongoManager.getInstance().openConnection();
 
+        // check if podcast exists
+        Podcast foundPodcast = this.podcastMongo.findPodcastById(podcastId);
+        if (foundPodcast == null) {
+            Logger.error("Podcast to delete not found");
+            return;
+        }
+
+        // update podcast
+        Boolean result = this.podcastMongo.deletePodcastById(podcastId);
+        if (result)
+            Logger.success("Podcast deleted");
+        else
+            Logger.error("Podcast not deleted");
+
+        MongoManager.getInstance().closeConnection();
+    }
+
+    public void addEpisode(Podcast podcast, Episode episode) {
+        MongoManager.getInstance().openConnection();
+
+        // check if podcast exists
+        Podcast foundPodcast = this.podcastMongo.findPodcastById(podcast.getId());
+        if (foundPodcast == null) {
+            Logger.error("Episode's podcast not found");
+            return;
+        }
+
+        // check if episode already exists
+        if (podcast.getEpisodes().contains(episode)) {
+            Logger.error("Episode already exists");
+            return;
+        }
+
+        // update podcast
+        podcast.addEpisode(episode);
+        Boolean result = this.podcastMongo.updatePodcast(podcast);
+        if (result) {
+            Logger.success("Episode added");
+        } else {
+            // reset the podcast object to the old status (for interface update)
+            Logger.error("Episode not added");
+            podcast.copy(foundPodcast);
+        }
+
+        MongoManager.getInstance().closeConnection();
+    }
+
+    public void deleteEpisode(Podcast podcast, Episode episode) {
+        MongoManager.getInstance().openConnection();
+
+        // check if podcast exists
+        Podcast foundPodcast = this.podcastMongo.findPodcastById(podcast.getId());
+        if (foundPodcast == null) {
+            Logger.error("Episode's podcast not found");
+            return;
+        }
+
+        // check if episode exists
+        if (podcast.getEpisodes().contains(episode))
+            Logger.success("Episode found");
+        else
+            Logger.error("Episode not found");
+
+        // update podcast
+        Boolean result = this.podcastMongo.deleteEpisodeOfPodcast(podcast.getId(), episode.getName());
+        if (result) {
+            Logger.success("Podcast deleted");
+            podcast.getEpisodes().remove(episode);
+        } else {
+            Logger.error("Podcast not deleted");
+        }
+
+        MongoManager.getInstance().closeConnection();
     }
 
     //-----------------------------------------------
