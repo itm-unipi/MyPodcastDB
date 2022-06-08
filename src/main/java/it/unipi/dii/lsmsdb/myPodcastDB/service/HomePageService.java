02604ca92e33b2f4e778f2f0149cc8c0cbaaaed0
Matteo Abaterusso <matteoabaterusso@gmail.com>
2022-06-08 15:16:48 +0200

    Retrieved Most Liked Podcast for Unregistered User

--- src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/service/HomePageService.java
+++ src/main/java/it/unipi/dii/lsmsdb/myPodcastDB/service/HomePageService.java
@@ -6,6 +6,7 @@ import it.unipi.dii.lsmsdb.myPodcastDB.model.Podcast;
 import it.unipi.dii.lsmsdb.myPodcastDB.model.User;
 import it.unipi.dii.lsmsdb.myPodcastDB.persistence.mongo.AuthorMongo;
 import it.unipi.dii.lsmsdb.myPodcastDB.persistence.mongo.MongoManager;
+import it.unipi.dii.lsmsdb.myPodcastDB.persistence.mongo.QueryMongo;
 import it.unipi.dii.lsmsdb.myPodcastDB.persistence.neo4j.AuthorNeo4j;
 import it.unipi.dii.lsmsdb.myPodcastDB.persistence.neo4j.Neo4jManager;
 import it.unipi.dii.lsmsdb.myPodcastDB.persistence.neo4j.PodcastNeo4j;
@@ -23,12 +24,14 @@ public class HomePageService {
 
     //----------------- BIAGIO ----------------------
     private final AuthorMongo authorMongoManager;
+    private final QueryMongo queryMongoManager;
     private final AuthorNeo4j authorNeo4jManager;
     private final UserNeo4j userNeo4jManager;
     private final PodcastNeo4j podcastNeo4jManager;
 
     public HomePageService() {
         this.authorMongoManager = new AuthorMongo();
+        this.queryMongoManager = new QueryMongo();
         this.authorNeo4jManager = new AuthorNeo4j();
         this.userNeo4jManager = new UserNeo4j();
         this.podcastNeo4jManager = new PodcastNeo4j();
@@ -189,7 +192,7 @@ public class HomePageService {
             topRated.addAll(resultsTopRated);
 
         // Loading Most Liked Podcasts
-        List<Pair<Podcast, Integer>> resultsMostLikedPodcasts = new ArrayList<>();
+        List<Pair<Podcast, Integer>> resultsMostLikedPodcasts = new ArrayList<>();/*
         resultsMostLikedPodcasts.add(new Pair<>(new Podcast("956e4ed48c7856b7f57f71ad", "Into the Black", "https://is4-ssl.mzstatic.com/image/thumb/Podcasts115/v4/82/e2/c1/82e2c132-2275-25e7-d0af-30b34110c892/mza_12455054422451763750.jpg/600x600bb.jpg"), 567));
         resultsMostLikedPodcasts.add(new Pair<>(new Podcast("76f2901e32ba8672e3ee121b", "Bearded Ruckus", "https://is1-ssl.mzstatic.com/image/thumb/Podcasts123/v4/c1/0d/ee/c10dee1f-8d15-47cb-a9bf-28adcbd22ea7/mza_6387225195375893490.jpg/600x600bb.jpg"), 1767));
         resultsMostLikedPodcasts.add(new Pair<>(new Podcast("416618a9d2bb7c74c7a5b779", "Podcast But Outside", "https://is4-ssl.mzstatic.com/image/thumb/Podcasts116/v4/43/2e/4f/432e4f2a-3363-74fe-88c7-8a4e5866e419/mza_14697479480010952634.jpg/600x600bb.jpg"), 667));
@@ -212,8 +215,10 @@ public class HomePageService {
         resultsMostLikedPodcasts.add(new Pair<>(new Podcast("f49f9d110c037f1f39a668fc", "HOLD UP! We need to talk", "https://is2-ssl.mzstatic.com/image/thumb/Podcasts118/v4/16/71/de/1671de46-df8b-9430-5b79-156954a147fe/mza_979148902187170876.jpg/600x600bb.jpg"), 853));
         resultsMostLikedPodcasts.add(new Pair<>(new Podcast("7f7f64a07837778030d8afc6", "Breaking Average", "https://is2-ssl.mzstatic.com/image/thumb/Podcasts125/v4/b5/8a/49/b58a49be-2621-a98b-f94a-9c4d62fc3a3c/mza_4437068013901004299.jpg/600x600bb.jpg"), 745));
         resultsMostLikedPodcasts.add(new Pair<>(new Podcast("46f7ac981cdefd61b4f8683d", "Angry Dad Podcast", "https://is4-ssl.mzstatic.com/image/thumb/Podcasts125/v4/df/9e/8f/df9e8f0b-a576-0897-1715-c7870a33f12d/mza_14276480442415215195.jpg/600x600bb.jpg"), 30));
+        */
 
-        if (resultsMostLikedPodcasts != null)
+        this.queryMongoManager.getMostLikedPodcast(resultsMostLikedPodcasts);
+        if (!resultsMostLikedPodcasts.isEmpty())
             mostLikedPodcasts.addAll(resultsMostLikedPodcasts);
 
         // Load Most Followed Author
